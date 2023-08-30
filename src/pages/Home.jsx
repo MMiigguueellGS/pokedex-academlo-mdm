277d04fbdc4917e8fe23f74f7c4a4ce7f2d96499
6CROONUX9 <jerchiton.97@gmail.com>
2023-08-29 23:42:19 -0400

    The change was made in the Home of the design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,25 +18,56 @@ export const Home = () => {
   };
 
   return (
-    <main className="min-h-screen grid grid-rows-[1fr_auto]">
+    <main className="min-h-screen grid grid-rows-[1fr_auto] ">
+      
+      {/* Card home
+      <section className={`m-auto w-[220px] text-center capitalize border-[5px] rounded-md ${
+        borderStylePokemonType[homeIdPokemons?.types[0]]
+      }`}>
+      <header
+        className={`h-[80px] relative mb-8 ${
+          bgStylePokemonType[homeIdPokemons?.types[0]]
+        }`}
+      >
+        <div className="absolute left-1/2 -translate-x-1/2 -bottom-4 h-[65px] aspect-square">
+          <img
+            className="h-full w-full object-contain"
+            src={homeIdPokemons?.image}
+            alt=""
+          />
+        </div>
+      </header>
       <section>
+        <h3 className={`text-lg font-bold ${nameStylePokemon[homeIdPokemons?.types[0]]}`}>{homeIdPokemons?.name}</h3>
+        <h4>{joinPokemonTypes(homeIdPokemons?.types)}</h4>
+        <h5 className="text-sm mb-2">Types</h5>
+        <hr />
+        <StatList stats={homeIdPokemons?.stats} />
+      </section>
+      </section> */}
+
+
+      <section className="flex flex-col justify-center items-center mx-2">
         <article>
-          <div>
+          {/* seccion superior */}
+          <div className="text-center">
             <img src="/images/banner.png" alt="" />
           </div>
-          <h2>!Hello trainer!</h2>
-          <p>To start give your name</p>
-          <form onSubmit={handleSubmit}>
+          <h2 className="text-red-500 text-center text-2xl">!Hello trainer!</h2>
+          <p className="text-center text-lg">To start give your name</p>
+          <form onSubmit={handleSubmit} className="flex justify-center">
             <input
               placeholder="your name..."
               id="nameTrainer"
               type="text"
               required
             />
-            <button>Start!</button>
+            <button className="px-3 rounded bg-red-500 hover:bg-red-600 text-white border-black">Start!</button>
           </form>
         </article>
       </section>
+
+      {/* parte principal inferior*/}
       <FooterPokeball />
     </main>
   );
